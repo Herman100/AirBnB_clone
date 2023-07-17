e78123c636a4955a39688eb299884681fdda8279
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-07-17 01:07:27 +0000

    Airbnb project, city file
    Co-authored-by: Herman Kwamebour<hermankwamebour30@gmail.com>

--- models/city.py
+++ models/city.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module represents a class City that inherits from BaseModel class"""
+"""This module represents a class City that inherits from BaseModel"""
 
 from models.base_model import BaseModel
 
